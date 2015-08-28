c94a2f25d694d9c47d7aae20c3befecd3eef967b
gmarler <gmarler@gmarler.com>
2015-08-28 12:30:35 -0400

    Add Live Templates, Get first tests to pass

--- spec/todo.js
+++ spec/todo.js
@@ -1,9 +1,31 @@
-describe('', function() {
-  
+describe('TODO List Tests', function() {
+  // scope - hold items on the controller
+  var scope = {};
+
   beforeEach(function() {
+    module('todo');
+    // inject - access angular controller
+    inject(function($controller) {
+      // $controller - initialize controller with test scope
+      $controller('TodoController',
+        { $scope:scope } );
+    });
   });
 
-  it('', function() {
+  it('defines a list object', function() {
+    expect(scope.list).toBeDefined();
   });
 
-})
+  it('defines list element #1', function() {
+    expect(scope.list[0]).toEqual('test');
+  });
+
+  it('defines list element #2', function() {
+    expect(scope.list[1]).toEqual('execute');
+  });
+
+  it('defines list element #3', function () {
+    expect(scope.list[2]).toEqual('refactor');
+  });
+
+});
